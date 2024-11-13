771391d6e63388442a31e6a06bd6ef8328fff160
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-11-13 17:57:50 -0500

    Initial Board

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface valuePropType {
+  value: number
+}
+
+function Square({value} : valuePropType) {
+  return <button className="square">{value}</button> 
+}
+
+function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div className="board-row">
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div className="board-row">
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board/>
     </div>
   );
 }
