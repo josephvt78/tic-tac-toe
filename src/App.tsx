24ddcf6f9af0f097982dde46f9ef45c6ae9cf6d2
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-11-13 18:53:29 -0500

    set value as 'X' in all the squares with refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,40 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function Square() {
-  
-  const [value, setValue] = useState<string | null>(null);
-  
-  function handleClick() {
-    setValue('X');
-  }
-  
-  return <button className="square" onClick={handleClick}>{value}</button> 
+interface SquareValue {
+  value: string | null;
+  onSquareClick: () => void;
+}
+
+function Square({value, onSquareClick} : SquareValue) {
+  return <button className="square" onClick={onSquareClick}>{value}</button> 
 }
 
 function Board() {
+  const [squares, setSquares] = useState(Array<string | null>(9).fill(null));
+
+  function handleClick(i: number) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );
